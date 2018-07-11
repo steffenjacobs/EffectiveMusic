426f2a4371070dc67f875550ea8a51c5e3e9a482
Steffen Jacobs <steffen.jacobs@hotmail.de>
2018-07-11 22:34:27 +0200

    cleanup of MusicController

--- src/main/java/me/steffenjacobs/effectivemusic/MusicController.java
+++ src/main/java/me/steffenjacobs/effectivemusic/MusicController.java
@@ -1,7 +1,5 @@
 package me.steffenjacobs.effectivemusic;
 
-import java.io.FileNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jlgui.basicplayer.BasicPlayerException;
 import me.steffenjacobs.effectivemusic.AudioPlayer.Status;
 import me.steffenjacobs.effectivemusic.domain.TrackDTO;
@@ -23,36 +20,36 @@ public class MusicController {
 	AudioPlayer audioPlayer;
 
 	@PostMapping(value = "/music/play", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public ResponseEntity<String> playSong(String path) throws BasicPlayerException, FileNotFoundException, JavaLayerException {
+	public ResponseEntity<String> playSong(String path) throws BasicPlayerException {
 		audioPlayer.playAudio(path);
 		return new ResponseEntity<String>("Playing file " + path, HttpStatus.ACCEPTED);
 	}
 
-	@PostMapping(value = "/music/stop", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	@PostMapping(value = "/music/stop")
 	public ResponseEntity<String> stop() throws BasicPlayerException {
 		audioPlayer.stop();
 		return new ResponseEntity<String>("Stopped music", HttpStatus.OK);
 	}
 
-	@PostMapping(value = "/music/pause", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	@PostMapping(value = "/music/pause")
 	public ResponseEntity<String> pauseSong() throws BasicPlayerException {
 		audioPlayer.pause();
 		return new ResponseEntity<String>("Paused", HttpStatus.OK);
 	}
 
-	@PostMapping(value = "/music/resume", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public ResponseEntity<String> resumeSong() throws BasicPlayerException, JavaLayerException {
+	@PostMapping(value = "/music/resume")
+	public ResponseEntity<String> resumeSong() throws BasicPlayerException {
 		audioPlayer.resume();
 		return new ResponseEntity<String>("Resumed", HttpStatus.OK);
 	}
 
-	@PostMapping(value = "/music/status", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public ResponseEntity<String> getStatus(String path) {
+	@GetMapping(value = "/music/status")
+	public ResponseEntity<String> getStatus() {
 		Status status = audioPlayer.getStatus();
 		return new ResponseEntity<String>("status: " + status, HttpStatus.OK);
 	}
 
-	@GetMapping(value = "/music/gain", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	@GetMapping(value = "/music/gain")
 	public ResponseEntity<String> getGain() {
 		double gain = audioPlayer.getGain();
 		return new ResponseEntity<String>("gain: " + gain, HttpStatus.OK);
@@ -64,7 +61,7 @@ public class MusicController {
 		return new ResponseEntity<String>("gain: " + gain, HttpStatus.OK);
 	}
 
-	@GetMapping(value = "/music/position", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	@GetMapping(value = "/music/position")
 	public ResponseEntity<String> getPosition() throws BasicPlayerException {
 		long position = audioPlayer.getMicrosecondPosition();
 		return new ResponseEntity<String>("position: " + position / 1000000 + "s", HttpStatus.OK);
@@ -76,7 +73,7 @@ public class MusicController {
 		return new ResponseEntity<String>("position: " + position, HttpStatus.OK);
 	}
 
-	@GetMapping(value = "/music/info", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	@GetMapping(value = "/music/info")
 	public ResponseEntity<TrackDTO> getTrackInfo() throws BasicPlayerException {
 		return new ResponseEntity<TrackDTO>(audioPlayer.getTrackInformation(), HttpStatus.OK);
 	}
